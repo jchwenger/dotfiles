cb4f08e359e1237269b1bc8375c12e0848640ec3
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-04-02 13:41:27 +0100

    updating backup to usb to use /media/default/JESH_S_USB var & take user input to specify target dir

--- utils/backup.sh
+++ utils/backup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
- if [ -d /media/$(whoami)/JESH_S_USB/coding/projects/ ]; then
+if [[ $#  -ne 1 ]]; then
+	echo "Please provide location folder, e.g. 'coding/projects'"
+else
+	target="$1"
+	echo "attempting backup to $usb/$target, creating dir(s) if necessary"
+fi
+
+if [ -d $usb ]; then
 	dir="$(pwd)"
-	cd dir;
-	base="$(basename "$dir")";
-	# up_dir="${dir%$base}";
+	cd dir
+	base="$(basename "$dir")"
+	# up_dir="${dir%$base}"
 
 	# echo "dir: $dir"
 	# echo "base: $base"
 	# echo "up dir: $up_dir"
 
-	cd ..;
-	zip --symlinks -r "$base".zip "$base";
+	cd ..
+	zip --symlinks -r "$base".zip "$base"
 	echo "moving zip to usb"
-	mv "$dir".zip /media/$(whoami)/JESH_S_USB/coding/projects/;
-	cd "$base";
+	mv "$dir".zip $usb/$target
+	cd "$base"
 else
-	echo "directory /media/$(whoami)/JESH_S_USB/coding/projects/ not found";
+	echo "usb stick $usb not found"
 fi
