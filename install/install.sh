43767bf5eef58b819b88c7d0dcb734a5daafa9e1
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-10-12 14:33:36 +0100

    install | mongodb

--- install/install.sh
+++ install/install.sh
@@ -601,3 +601,15 @@ sudo apt update && sudo apt install --no-install-recommends yarn
 # sudo halt, then do:
 # systemctl list-jobs
 
+# mongodb
+# -------
+# https://www.mongodb.com/
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+# in case of an error, do this:
+# sudo apt-get install gnupg
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
