00f6503e16b5d085fbe3691b6563c61681c52eb4
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-10-12 14:37:26 +0100

    utils | split file into n equal parts

---
+++ utils/split.sh
@@ -0,0 +1,8 @@
+# -d : numeric suffixes
+# -n : number of files
+# then specify the end file (SOURCE.) wtih a dot
+# then add final .txt suffix after numbering
+
+# split -d -n number-of-splits \
+#   SOURCE.TXT SOURCE. \
+#   --additional-suffix .txt
