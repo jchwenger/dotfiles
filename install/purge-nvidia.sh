4e035700b967236500461805b32fb76ce329de56
jchwenger <34098722+jchwenger@users.noreply.github.com>
2022-01-27 23:08:43 +0000

    install | tensorflow & nvidia business

--- install/purge-nvidia.sh
+++ install/purge-nvidia.sh
@@ -1,13 +1,28 @@
 # in case there are conflicting versions of nvidia & cuda drivers
 
+# https://askubuntu.com/a/530050/1092704
 # https://askubuntu.com/a/530050
-sudo apt-get remove --auto-remove nvidia-cuda-toolkit -y
+sudo apt-get purge --auto-remove nvidia-cuda-toolkit -y
 
 # https://askubuntu.com/a/1077063
-sudo rm /etc/apt/sources.list.d/cuda*
+# sudo rm /etc/apt/sources.list.d/cuda*
+
+# https://github.com/tensorflow/tensorflow/issues/43236#issuecomment-693244342
+# To start fresh, clean up all the nivida-related packages. Be careful when using the same system as a desktop!
+sudo apt-get --purge remove 'cuda*'
+sudo apt-get --purge remove 'nvidia*'
+sudo apt-get --purge remove 'libnvidia*'
+sudo apt-get --purge remove 'libcudnn*'
+sudo apt-get --purge remove 'libnccl*'
+sudo apt-get --purge remove 'nccl*'
+
+# Check if all clean
+# sudo find /usr/local/cuda/ -name '*blas*'
+# sudo find /usr/lib/ -name '*blas*'
 
 # https://askubuntu.com/a/1180417
-sudo apt-get --purge -y remove 'cuda*' 'nvidia*'
+# sudo apt-get --purge -y remove 'cuda*' 'nvidia*'
+
 sudo reboot
 
 # fix divergences:
--- install/tensorflow-gpu-1.sh
+++ install/tensorflow-gpu-1.sh
@@ -8,10 +8,25 @@ sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
 sudo apt-get update
 wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
 sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+
+# https://github.com/tensorflow/tensorflow/issues/43236
+# CUDA 10.1 instructions for creating a locally available repo and installing from it
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+# sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-repo-ubuntu1804-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
+# sudo dpkg -i cuda-repo-ubuntu1804-10-1-local-10.1.243-418.87.00_1.0-1_amd64.deb
+# sudo apt-key add /var/cuda-repo-10-1-local-10.1.243-418.87.00/7fa2af80.pub
+
 sudo apt autoremove -y
 sudo apt-get update
 
 # Install NVIDIA driver
-sudo apt-get install --no-install-recommends nvidia-driver-430
+sudo apt-get install --no-install-recommends nvidia-driver-450
+
+# sudo apt-get install nvidia-cuda-toolkit -y
+
+# NIVIDIA bs
+sudo dpkg -i nccl-repo-ubuntu1804-2.6.4-ga-cuda10.1_1-1_amd64.deb # must have downloaded it first from NVIDIA!
+sudo apt install libnccl2 libnccl-dev
 
 # Reboot. Check that GPUs are visible using the command: nvidia-smi
