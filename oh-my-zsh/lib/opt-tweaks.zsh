14d5a5adcf7d3d3686ec1bde0f4570d98b0a0cfa
jchwenger <34098722+jchwenger@users.noreply.github.com>
2022-04-14 22:33:05 +0100

    oh my | lib, opt tweaks

---
+++ oh-my-zsh/lib/opt-tweaks.zsh
@@ -0,0 +1,18 @@
+# see .oh-my-zsh/lib/completion.sh
+
+setopt menu_complete   # do not autoselect the first completion entry
+unsetopt auto_menu
+
+# https://unix.stackexchange.com/a/273863
+setopt bang_hist                 # Treat the '!' character specially during expansion.
+setopt extended_history          # Write the history file in the ":start:elapsed;command" format.
+setopt inc_append_history        # Write to the history file immediately, not when the shell exits.
+setopt share_history             # Share history between all sessions.
+setopt hist_expire_dups_first    # Expire duplicate entries first when trimming history.
+setopt hist_ignore_dups          # Don't record an entry that was just recorded again.
+setopt hist_ignore_all_dups      # Delete old recorded entry if new entry is a duplicate.
+setopt hist_find_no_dups         # Do not display a line previously found.
+setopt hist_ignore_space         # Don't record an entry starting with a space.
+setopt hist_save_no_dups         # Don't write duplicate entries in the history file.
+setopt hist_reduce_blanks        # Remove superfluous blanks before recording entry.
+setopt hist_verify               # Don't execute immediately upon history expansion.
