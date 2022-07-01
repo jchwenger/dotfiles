920efd9aa3fc3264c50d1a1286bd1a91fdca6c80
jchwenger <34098722+jchwenger@users.noreply.github.com>
2022-07-01 17:03:10 +0100

    setup | zsh-rvm-lazy

--- install/setup.sh
+++ install/setup.sh
@@ -9,7 +9,10 @@ ln -fs  ~/dotfiles/.vimrc ~/.vimrc
 # extended vim mode
 # git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 
-vim ~/.zshrc '+:execute ":silent! %s/plugins=(git)/export NVM_LAZY=1\rplugins=(git vi-mode fzf gcloud docker docker-compose gh fd aws nvm)/e | wq"'
+# rvm lazy
+git clone https://github.com/FrederickGeek8/zsh-rvm-lazy ~/.oh-my-zsh/custom/plugins/zsh-rvm-lazy
+
+vim ~/.zshrc '+:execute ":silent! %s/plugins=(git)/export NVM_LAZY=1\rplugins=(git vi-mode fzf gcloud docker docker-compose gh fd aws nvm zsh-rvm-lazy)/e | wq"'
 vim ~/.zshrc '+:execute "norm /^sourceosource ~/.zshrc_src:wq"'
 
 # fix for current repo
