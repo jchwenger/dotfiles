e482d84d2af5820206ad60f3af5cbbd205858364
jchwenger <34098722+jchwenger@users.noreply.github.com>
2022-10-15 11:30:38 +0100

    oh my | term title without user

--- oh-my-zsh/lib/termsupport.zsh
+++ oh-my-zsh/lib/termsupport.zsh
@@ -42,7 +42,8 @@ function title {
 }
 
 ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
-ZSH_THEME_TERM_TITLE_IDLE="%2~ | %n@%m"
+# ZSH_THEME_TERM_TITLE_IDLE="%2~ | %n@%m"
+ZSH_THEME_TERM_TITLE_IDLE="%2~"
 # Avoid duplication of directory in terminals with independent dir display
 if [[ "$TERM_PROGRAM" == Apple_Terminal ]]; then
   ZSH_THEME_TERM_TITLE_IDLE="%n@%m"
