243346bc1140def3a357411c5c11d8205e499627
jchwenger <34098722+jchwenger@users.noreply.github.com>
2022-10-15 10:16:50 +0100

    install | purge nvidia updated

--- install/purge-nvidia.sh
+++ install/purge-nvidia.sh
@@ -2,19 +2,19 @@
 
 # https://askubuntu.com/a/530050/1092704
 # https://askubuntu.com/a/530050
-sudo apt-get purge --auto-remove nvidia-cuda-toolkit -y
+# sudo apt-get purge --auto-remove nvidia-cuda-toolkit -y
 
 # https://askubuntu.com/a/1077063
 # sudo rm /etc/apt/sources.list.d/cuda*
 
 # https://github.com/tensorflow/tensorflow/issues/43236#issuecomment-693244342
 # To start fresh, clean up all the nivida-related packages. Be careful when using the same system as a desktop!
-sudo apt-get --purge remove 'cuda*'
-sudo apt-get --purge remove 'nvidia*'
-sudo apt-get --purge remove 'libnvidia*'
-sudo apt-get --purge remove 'libcudnn*'
-sudo apt-get --purge remove 'libnccl*'
-sudo apt-get --purge remove 'nccl*'
+# sudo apt-get --purge remove 'cuda*'
+# sudo apt-get --purge remove 'nvidia*'
+# sudo apt-get --purge remove 'libnvidia*'
+# sudo apt-get --purge remove 'libcudnn*'
+# sudo apt-get --purge remove 'libnccl*'
+# sudo apt-get --purge remove 'nccl*'
 
 # Check if all clean
 # sudo find /usr/local/cuda/ -name '*blas*'
@@ -23,9 +23,17 @@ sudo apt-get --purge remove 'nccl*'
 # https://askubuntu.com/a/1180417
 # sudo apt-get --purge -y remove 'cuda*' 'nvidia*'
 
-sudo reboot
+# sudo reboot
 
 # fix divergences:
 # https://askubuntu.com/a/1041703
-LC_MESSAGES=C dpkg-divert --list '*nvidia-340*' | sed -nre 's/^diversion of (.*) to .*/\1/p' | xargs -rd'\n' -n1 -- sudo dpkg-divert --remove
-sudo apt --fix-broken install
+# LC_MESSAGES=C dpkg-divert --list '*nvidia-340*' | sed -nre 's/^diversion of (.*) to .*/\1/p' | xargs -rd'\n' -n1 -- sudo dpkg-divert --remove
+# sudo apt --fix-broken install
+
+# https://stackoverflow.com/a/62276101
+sudo apt-get --purge remove "*cublas*" "cuda*" "nsight*"
+sudo apt-get --purge remove "*nvidia*"
+sudo rm -rf /usr/local/cuda*
+
+# remove nvidia from here
+sudo vim /etc/apt/sources.list
