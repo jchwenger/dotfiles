d1325c2359a72545833d2265d14d5f3afefef45e
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-06-26 12:42:13 +0100

    zsh, utils | lp{v,h}txt to their own individual scripts in utils

---
+++ utils/lpvtxt.sh
@@ -0,0 +1,20 @@
+total=""
+for f in $*
+do
+  if [[ $f =~ ${@: -1} ]]
+  then
+    total="$total$(cat $f)"
+  else
+    total="$total$(cat $f)\f"
+  fi
+done
+echo $total \
+| lp \
+    -o media=a4 \
+    -o cpi=12 \
+    -o lpi=7.2 \
+    -o page-left=48 \
+    -o page-right=48 \
+    -o page-top=48 \
+    -o page-bottom=48 \
+    -o sides=two-sided-long-edge
