c0a6b2717e868abd292344b39154018b4ceb1451
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-12-24 17:06:01 +0100

    utils | ffmpeg, including title data, chatgpt update

--- utils/ffmpeg-split-by-chapters.sh
+++ utils/ffmpeg-split-by-chapters.sh
@@ -1,15 +1,27 @@
-#!/bin/sh -efu
-
-# https://stackoverflow.com/a/53553938
-
 input="$1"
-ffprobe \
-    -print_format csv \
+
+# Extract the file title and chapter information
+metadata_info=$(ffprobe \
+    -v error \
+    -print_format json \
+    -show_format \
     -show_chapters \
-    "$input" |
-cut -d ',' -f '5,7,8' |
-while IFS=, read start end chapter
-do
+    "$input")
+
+file_title=$(echo "$metadata_info" | jq -r '.format.tags.title // "Untitled File"')
+
+track_number=1
+
+# Process each chapter
+echo "$metadata_info" | jq -c '.chapters[]' | while read -r chapter_data; do
+    start=$(echo "$chapter_data" | jq -r '.start_time')
+    end=$(echo "$chapter_data" | jq -r '.end_time')
+    chapter_title=$(echo "$chapter_data" | jq -r '.tags.title // "Untitled Chapter"')
+
+    # Combine file title and chapter title
+    combined_title="${file_title} - ${chapter_title}"
+
+    # Process each chapter with ffmpeg
     ffmpeg \
         -nostdin \
         -ss "$start" -to "$end" \
@@ -17,5 +29,10 @@ do
         -c copy \
         -map 0 \
         -map_chapters -1 \
-        "${input%.*} $chapter.${input##*.}"
+        -metadata title="$combined_title" \
+        -metadata track="$track_number" \
+        "${input%.*} - ${chapter_title}.${input##*.}"
+
+    # Updating the track number
+    track_number=$((track_number + 1))
 done
